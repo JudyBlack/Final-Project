2f0bea3cda12815faa41d5b9a5894f33c98901d2
JudyBlack <126619390+JudyBlack@users.noreply.github.com>
2025-06-12 13:48:56 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,36 @@
 import React from 'react';
 import "./App.scss";
-import { Route, Routes } from 'react-router-dom';
+import { Route, Router, Routes } from 'react-router-dom';
 
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import HomePage from './Pages/HomePage/HomePage';
 import MoviePage from './Pages/MoviePage/MoviePage';
+import TvSeriesPage from './Pages/TvSeriesPage/TvSeriesPage';
 import Login from './Pages/login/Login';
-
-
+import { BrowserRouter } from 'react-router-dom';
+import MovieDetails from './components/Details/Movie/MovieDetails';
+import TvSeriesDetails from './components/Details/Tv/TvDetails';
 
 
 
 export function App() {
   return (
+    
     <div className='App'>
+      <BrowserRouter>
       <Header />
       <Routes>
         <Route path='/' element={<HomePage/> } />
         <Route path='login' element={<Login/> } />
-        <Route path='moviePage' element={<MoviePage /> } />    
-      </Routes>
-      
-      
-
-      
+        <Route path='moviePage' element={<MoviePage /> } />  
+        <Route path="/tvseries" element={<TvSeriesPage />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
+        <Route path="/tv/:id" element={<TvSeriesDetails />} />
+      </Routes> 
+      </BrowserRouter>  
     </div>
+    
   )
 };
-
 
